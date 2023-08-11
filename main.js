46e445a586b44f5bbd671baca7f330dbfc8b5f98
Franek <franio.blaszkowski@gmail.com>
2023-08-11 15:04:40 +0200

    BEM & DOM

--- main.js
+++ main.js
@@ -5,3 +5,11 @@ const firstName = 'Franek';
 
 
 console.log(`Nazywam się ${firstName} i mam ${age} lat`);
+
+const header = document.querySelector('.page-header__heading--js')
+
+header.innerHTML = `Nazywam się ${firstName} i mam ${age} lat`
+
+console.log(header.style)
+
+header.style.color = 'red';
